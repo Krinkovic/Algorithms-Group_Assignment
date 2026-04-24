192d82739505a0fcea7a92be918809d2e1e78c82
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2026-04-24 09:14:32 +0200

    Added an efficiency improvement to bubble sort I had missed

--- src/sorting.c
+++ src/sorting.c
@@ -5,7 +5,7 @@
 void bubbleSort(unsigned int arr[], int n, int *op)
 {
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1; j++) {
+        for (int j = 0; j < n - 1 - i; j++) {
             if (arr[j + 1] < arr[j]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
